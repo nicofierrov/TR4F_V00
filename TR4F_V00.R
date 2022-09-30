55afacbd9c67699f1f682c7353192cbd4a5c9b71
Nicolas Fierro Viedma <nicofierrov@gmail.com>
2022-09-30 16:55:25 +0200

    First commit

---
+++ TR4F_V00.R
@@ -0,0 +1,14 @@
+
+# INICIO ------------------------------------------------------------------
+
+# Exploracion data final
+# Primero pensar como resolver el problema
+
+rm(list = ls())
+graphics.off()
+opar <- par()
+
+
+# LIBRERIAS ---------------------------------------------------------------
+
+
